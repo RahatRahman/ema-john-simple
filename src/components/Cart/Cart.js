fa5822e6b7ec06518247e57f381818edaf0df158
RahatRahman <mahmudur665@gmail.com>
2020-09-02 16:31:01 +0600

    added till review items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Cart = (props) => {
   const cart = props.cart;
@@ -32,6 +33,10 @@ const Cart = (props) => {
         <small>Tax + VAT: ${tax}</small>
       </p>
       <p>Total Price: ${grandTotal}</p>
+      <br />
+      <Link to="/review">
+        <button className="main-button">Review Order</button>
+      </Link>
     </div>
   );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,21 +3,30 @@ import fakeData from "../../fakeData";
 import "./Shop.css";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDatabaseCart } from "../../utilities/databaseManager";
 
 const Shop = () => {
   const first10 = fakeData.slice(0, 10);
-  const [products, setProducts] = useState(first10);
+  const [products] = useState(first10);
   const [cart, setCart] = useState([]);
 
   const handleAddProduct = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    const sameProduct = newCart.filter((pd) => pd.key === product.key);
+    const count = sameProduct.length;
+    addToDatabaseCart(product.key, count);
   };
   return (
     <div className="shop-container">
       <div className="product-container">
         {products.map((product) => (
-          <Product handleAddProduct={handleAddProduct} product={product}></Product>
+          <Product
+            showAddToCart={true}
+            handleAddProduct={handleAddProduct}
+            product={product}
+            key={product.key}
+          ></Product>
         ))}
       </div>
       <div className="cart-container">
